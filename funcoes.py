f2a7a1fb0ad80cd82f1275775c028338af6d83fb
Bruno Trindade <bruuno.trindade@gmail.com>
2019-05-24 09:44:32 -0500

    mm1k e mmsk atualizadas

--- funcoes.py
+++ funcoes.py
@@ -26,7 +26,7 @@ def mms():
     if p < 1:
         soma = 0
         #somatorio
-        for n in range(0,S+1):
+        for n in range(0,s):
             soma += (S*p)**n / math.factorial(n)
 
         p0 = ((( S**S * p**(S+1) ) / (math.factorial(S)*(1-p))) + soma ) ** (-1)
@@ -44,8 +44,49 @@ def mms():
 
 #FILA M/M/1/K
 def mm1k():
-    print ("Just a test")
+    lamb = double(input("Lambda: "))
+    u    = double(input("Mi: "))
+    t    = double(input("Tempo (0, caso o tempo nao tenha sido informado):  "))
+    k    = int(input("K(Capacidade do sistema: "))    
+    p = lamb/u
+    lambli = lamb*(1-(p*k))
+    
+    if p == 1:
+       ls = k/2
+    else:   
+        ls = p/(1-p) - ((k+1)*p**(k+1))/ 1 - p**(k+1)
+    
+    ws = ls/lambli
+    wq = ws - 1/u
+    lq = wq*lambli
 
+    print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n".format(p,lq,ls,wq,ws))
 #FILA M/M/S/K
 def mmsk():
-    pass
+    lamb = double(input("Lambda: "))
+    u    = double(input("Mi: "))
+    s    = int(input("S (atendentes):  "))
+    t    = double(input("Tempo (0, caso o tempo nao tenha sido informado):  "))
+    p = lamb/(s*u)
+    lambli = lamb*(1-(p*k))
+
+    if p != 1:
+        soma = 0
+        #somatorio
+        for n in range(0,s):
+            soma += (s*p)**n / math.factorial(n)
+
+        p0 = ((( s**s * p**(s+1) ) / (math.factorial(s)*(1-p))) + soma ) ** (-1)
+    else:
+        soma = 0
+        for n in range(0,s):
+            soma += (s**s) / math.factorial(n)
+
+        p0 =    ((s**s)/math.factorial(s))*(k-s) + soma
+
+    lq = (( s**s * p**(s+1) ) / math.factorial(s)*((1-p)**2))*((1-p)*(k-s)*(p**(k-s))*p0
+    wq = lq/lambli
+    ws = wq + 1/u
+    ls = lamli*ws
+
+    print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n p0: {:.4f} \n".format(p,lq,ls,wq,ws,p0))
