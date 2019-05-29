32f15721a19fa41450c1b10fb89e543faa1a20e3
André Gomes <andregomesngr@gmail.com>
2019-05-29 08:20:00 -0500

    mm1k e mmsk prontas

--- funcoes.py
+++ funcoes.py
@@ -62,23 +62,26 @@ def mm1k(help):
     t    = float(input("Tempo" + (" (0, caso o tempo nao tenha sido informado):  " if help else ": ")))
     k    = int(input("K" + (" (capacidade do sistema): " if help else ": ")))
     p = lamb/u
-    lambli = lamb*(1-(p*k))
+    p0 = 0.0
+
 
     if p == 1:
        ls = k/2
+       p0 = 1/(k+1)
     else:
         ls = p/(1-p) - ((k+1)*p**(k+1))/ 1 - p**(k+1)
+        p0 = (1-p)/(1-(p**(k+1)))
 
+    pK = ((p**(k))*(1-p))/(1-(p**(k+1)))
+    lambli = lamb*(1-(pK))
     ws = ls/lambli
     wq = ws - 1/u
     lq = wq*lambli
 
-    print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n".format(p,lq,ls,wq,ws))
-
     if help:
-        print("\n P (Probabilidade de se manter ocupado): {:.4f} \n Lq (Comprimento medio da fila): {:.4f} \n Ls (Comprimento medio do sistema): {:.4f} \n Wq (Tempo medio de espera na fila): {:.4f} \n Ws (Tempo medio de espera no sistema): {:.4f} \n".format(p,lq,ls,wq,ws))
+        print("\n P (Probabilidade de se manter ocupado): {:.4f} \n Lq (Comprimento medio da fila): {:.4f} \n Ls (Comprimento medio do sistema): {:.4f} \n Wq (Tempo medio de espera na fila): {:.4f} \n Ws (Tempo medio de espera no sistema): {:.4f} \n p0 (Probabilidade de nao ter ninguem): {:.4f} \n".format(p,lq,ls,wq,ws,p0))
     else:
-        print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n".format(p,lq,ls,wq,ws))
+        print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n p0: {:.4f}".format(p,lq,ls,wq,ws,p0))
 
 #FILA M/M/S/K
 def mmsk(help):
@@ -89,7 +92,6 @@ def mmsk(help):
     k    = int(input("K" + (" (capacidade do sistema): " if help else ": ")))
     t    = float(input("Tempo" + (" (0, caso o tempo nao tenha sido informado): " if help else ": ")))
     p = lamb/(s*u)
-    lambli = lamb*(1-(p*k))
     p0 = 0.0
 
     if p != 1:
@@ -98,21 +100,22 @@ def mmsk(help):
         for n in range(0,s+1):
             soma += (s*p)**n / math.factorial(n)
 
-        p0 = ((( s**s * p**(s+1) ) / (math.factorial(s)*(1-p))) + soma ) ** (-1)
+        p0 = ((( (s**s) * (p**(s+1)) * (1-(p**(k-s))) ) / (math.factorial(s)*(1-p))) + soma ) ** (-1)
     else:
         #somatorio
         soma = 0.0
         for n in range(0,s+1):
-            soma += (s**s) / math.factorial(n)
+            soma += (s**n) / math.factorial(n)
 
-        p0 =    ((s**s)/math.factorial(s))*(k-s) + soma
+        p0 = (((s**s)/math.factorial(s))*(k-s) + soma)**(-1)
 
+    pK = ((p**(k))*(1-p))/(1-(p**(k+1)))
+    lambli = lamb*(1-(pK))
     lq = (( s**s * p**(s+1) ) / math.factorial(s)*((1-p)**2))*((1-p)*(k-s)*(p**(k-s)))*p0
     wq = lq/lambli
     ws = wq + 1/u
     ls = lambli*ws
 
-    print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n p0: {:.4f} \n".format(p,lq,ls,wq,ws,p0))
     if help:
         print("\n P (Probabilidade de se manter ocupado): {:.4f} \n Lq (Comprimento medio da fila): {:.4f} \n Ls (Comprimento medio do sistema): {:.4f} \n Wq (Tempo medio de espera na fila): {:.4f} \n Ws (Tempo medio de espera no sistema): {:.4f} \n p0 (Probabilidade de nao ter ninguem): {:.4f} \n".format(p,lq,ls,wq,ws,p0))
     else:
