94f409267e7499f764b53ba96fe1bb45cf9ecdd8
André Gomes <andregomesngr@gmail.com>
2019-05-30 10:44:51 -0500

    Cálculo do Pn finalizado

--- funcoes.py
+++ funcoes.py
@@ -10,6 +10,7 @@ def mm1(help, ler_p):
     lamb = float(input("Lambda" + (" (taxa media de chegada): " if help else ": ")))
     u    = float(input("Mi" + (" (taxa media de clientes atendidos por servidor): " if help else ": ")))
     t    = float(input("Tempo" + (" (0, caso o tempo nao tenha sido informado):  " if help else ": ")))
+
     p    = lamb/u
     ls   = p/(1-p)
     lq   = (p**2)/(1-p)
@@ -19,10 +20,10 @@ def mm1(help, ler_p):
     wqt  = p * wst
 
     if help:
-        print("\n P (Probabilidade de se manter ocupado): {:.4f} \n Lq (Comprimento medio da fila): {:.4f} \n Ls (Comprimento medio do sistema): {:.4f} \n Wq (Tempo medio de espera na fila): {:.4f} \n Ws (Tempo medio de espera no sistema): {:.4f} \n Wst (Probabilidade de permanecer 't' unidades de tempo no sistema): {:.4f} % \n Wqt (Probabilidade de permanecer 't' unidades de tempo na fila): {:.4f} % \n".format(p,lq,ls,wq,ws, wst, wqt))
+        print("\n P (Probabilidade de se manter ocupado): {:.4f} \n Lq (Comprimento medio da fila): {:.4f} \n Ls (Comprimento medio do sistema): {:.4f} \n Wq (Tempo medio de espera na fila): {:.4f} \n Ws (Tempo medio de espera no sistema): {:.4f} \n Wst (Probabilidade de permanecer 't' unidades de tempo no sistema): {:.4f} % \n Wqt (Probabilidade de permanecer 't' unidades de tempo na fila): {:.4f} % \n".format(p,lq,ls,wq,ws,wst,wqt))
 
     else:
-        print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n Ws(t): {:.4f} % \n Wq(t): {:.4f} % \n".format(p,lq,ls,wq,ws, wst, wqt))
+        print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n Ws(t): {:.4f} % \n Wq(t): {:.4f} % \n".format(p,lq,ls,wq,ws,wst,wqt))
 
     if ler_p:
         if help:
@@ -33,7 +34,7 @@ def mm1(help, ler_p):
         while True:
             try:
                 n = int(input("n: "))
-                pn = (p**n) * (1 - p) 
+                pn = (p**n) * (1 - p)
                 print("P{:d}: {:.4f} ".format(n, pn))
             except ValueError:
                 break
@@ -45,6 +46,7 @@ def mms(help, ler_p):
     u    = float(input("Mi" + (" (taxa media de clientes atendidos por servidor): " if help else ": ")))
     S    = int(input("S" + (" (atendentes):  " if help else ": ")))
     t    = float(input("Tempo" + (" (0, caso o tempo nao tenha sido informado):  " if help else ": ")))
+
     p    = lamb/(S*u)
 
     if p < 1:
@@ -65,11 +67,11 @@ def mms(help, ler_p):
     wqt  = (( ( ((S*p)**S) * p0 ) / (math.factorial(S) * (1-p))) * exp( -S*u*t*(1-p) )) * 100
 
     if help:
-        print("\n P (Probabilidade de se manter ocupado): {:.4f} \n Lq (Comprimento medio da fila): {:.4f} \n Ls (Comprimento medio do sistema): {:.4f} \n Wq (Tempo medio de espera na fila): {:.4f} \n Ws (Tempo medio de espera no sistema): {:.4f} \n p0 (Probabilidade de nao ter ninguem): {:.4f} \n Wst (Probabilidade de permanecer 't' unidades de tempo no sistema): {:.4f} % \n Wqt (Probabilidade de permanecer 't' unidades de tempo na fila): {:.4f} % \n".format(p,lq,ls,wq,ws,p0, wst, wqt))
+        print("\n P (Probabilidade de se manter ocupado): {:.4f} \n Lq (Comprimento medio da fila): {:.4f} \n Ls (Comprimento medio do sistema): {:.4f} \n Wq (Tempo medio de espera na fila): {:.4f} \n Ws (Tempo medio de espera no sistema): {:.4f} \n p0 (Probabilidade de nao ter ninguem): {:.4f} \n Wst (Probabilidade de permanecer 't' unidades de tempo no sistema): {:.4f} % \n Wqt (Probabilidade de permanecer 't' unidades de tempo na fila): {:.4f} % \n".format(p,lq,ls,wq,ws,p0,wst,wqt))
     else:
-        print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n p0: {:.4f} \n Ws(t): {:.4f} % \n Wq(t): {:.4f} % \n".format(p,lq,ls,wq,ws,p0, wst, wqt))
+        print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n p0: {:.4f} \n Ws(t): {:.4f} % \n Wq(t): {:.4f} % \n".format(p,lq,ls,wq,ws,p0,wst,wqt))
 
-    """if ler_p:
+    if ler_p:
         if help:
             print("Calculo de Pn (Probabilidade de ter 'n' no sistema): ")
         else:
@@ -78,10 +80,14 @@ def mms(help, ler_p):
         while True:
             try:
                 n = int(input("n: "))
-                pn = (p**n) * (1 - p) 
+                pn = 0.0
+                if n <= S :
+                    pn = (((S*p)**n)/math.factorial(n))*p0
+                else:
+                    pn = (((S**S)*(p**n))/math.factorial(S))*p0
                 print("P{:d}: {:.4f} ".format(n, pn))
             except ValueError:
-                break"""
+                break
 
 #FILA M/M/1/K
 def mm1k(help, ler_p):
@@ -90,6 +96,7 @@ def mm1k(help, ler_p):
     u    = float(input("Mi" + (" (taxa media de clientes atendidos por servidor): " if help else ": ")))
     t    = float(input("Tempo" + (" (0, caso o tempo nao tenha sido informado):  " if help else ": ")))
     k    = int(input("K" + (" (capacidade do sistema): " if help else ": ")))
+
     p = lamb/u
     p0 = 0.0
 
@@ -112,7 +119,7 @@ def mm1k(help, ler_p):
     else:
         print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n p0: {:.4f}".format(p,lq,ls,wq,ws,p0))
 
-    """if ler_p:
+    if ler_p:
         if help:
             print("Calculo de Pn (Probabilidade de ter 'n' no sistema): ")
         else:
@@ -121,10 +128,14 @@ def mm1k(help, ler_p):
         while True:
             try:
                 n = int(input("n: "))
-                pn = (p**n) * (1 - p) 
-                print("P{:d}: {:.4f} ".format(n, pn))
+                pn = 0.0
+                if p == 1:
+                    pn = 1/(k+1)
+                else:
+                    pn = ((p**n)*(1-p))/(1-(p**(k+1)))
+                print("P{:d}: {:.4f}".format(n,pn))
             except ValueError:
-                break"""
+                break
 
 #FILA M/M/S/K
 def mmsk(help, ler_p):
@@ -134,6 +145,7 @@ def mmsk(help, ler_p):
     s    = int(input("S" + (" (atendentes):  " if help else ": ")))
     k    = int(input("K" + (" (capacidade do sistema): " if help else ": ")))
     t    = float(input("Tempo" + (" (0, caso o tempo nao tenha sido informado): " if help else ": ")))
+
     p = lamb/(s*u)
     p0 = 0.0
 
@@ -164,7 +176,7 @@ def mmsk(help, ler_p):
     else:
         print("\n P: {:.4f} \n Lq: {:.4f} \n Ls: {:.4f} \n Wq: {:.4f} \n Ws: {:.4f} \n p0: {:.4f} \n".format(p,lq,ls,wq,ws,p0))
 
-    """if ler_p:
+    if ler_p:
         if help:
             print("Calculo de Pn (Probabilidade de ter 'n' no sistema): ")
         else:
@@ -173,7 +185,11 @@ def mmsk(help, ler_p):
         while True:
             try:
                 n = int(input("n: "))
-                pn = (p**n) * (1 - p) 
+                pn = 0.0
+                if n <= S :
+                    pn = (((S*p)**n)/math.factorial(n))*p0
+                elif n <= k
+                    pn = (((S**S)*(p**n))/math.factorial(S))*p0                                    
                 print("P{:d}: {:.4f} ".format(n, pn))
             except ValueError:
-                break"""
+                break
